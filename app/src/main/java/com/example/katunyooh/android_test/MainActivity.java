95918d602cbc501a380bc7c0c284332bf4dae6a9
katunyoo <katunyoo11@gmail.com>
2017-04-18 15:16:48 +0700

    finish gps

--- app/src/main/java/com/example/katunyooh/android_test/MainActivity.java
+++ app/src/main/java/com/example/katunyooh/android_test/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.katunyooh.android_test;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -12,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText editTextUserName;
     private EditText editTextPassword;
+    static final Integer LOCATION = 0x1;
 
     public static final String USER_NAME = "USERNAME";
 
@@ -21,10 +26,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         editTextUserName = (EditText) findViewById(R.id.editTextUserName);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
     }
 
     public void invokeLogin(View view) {
@@ -58,5 +63,30 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+protected void onStart()
+{
+    super.onStart();
+    askForPermission(Manifest.permission.ACCESS_FINE_LOCATION,LOCATION);
+}
+
+    private void askForPermission(String permission, Integer requestCode) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, permission)) {
+
+                //This is called if user has denied the permission before
+                //In this case I am just asking the permission again
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+
+            } else {
+
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+            }
+        } else {
+            //Toast.makeText(this, "" + permission + " is already granted.", Toast.LENGTH_SHORT).show();
+
+        }
+    }
 
 }
--- app/src/main/java/com/example/katunyooh/android_test/ShowSubjectDetail.java
+++ app/src/main/java/com/example/katunyooh/android_test/ShowSubjectDetail.java
@@ -1,6 +1,13 @@
 package com.example.katunyooh.android_test;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -12,6 +19,10 @@ import org.json.JSONObject;
 
 public class ShowSubjectDetail extends AppCompatActivity {
 
+    private TextView txtLat, txtLng;
+    private LocationManager objLocationManager;
+    private Criteria objCriteria;
+    private boolean bolGPS, bolNetwork;
     private String subjectString;
     private String usernameString;
     private String tag = "10AprilV3";
@@ -23,7 +34,11 @@ public class ShowSubjectDetail extends AppCompatActivity {
         setContentView(R.layout.activity_show_subject_detail);
 
 
+//Bind Widget
+        bindWidget();
 
+        //Open Service
+        openService();
 
 
         //Get Value
@@ -94,4 +109,120 @@ public class ShowSubjectDetail extends AppCompatActivity {
         Toast.makeText(ShowSubjectDetail.this,"เช็คชื่อไม่สำเร็จ กรุณาตรวจสอบพื้นที่", Toast.LENGTH_SHORT).show();
 
     }
+
+    public void logoutApp(View view){
+        Intent intent = new Intent(ShowSubjectDetail.this, MainActivity.class);
+        finish();
+        startActivity(intent);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        bolGPS = objLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+
+        if (!bolGPS) {
+            bolNetwork = objLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (!bolNetwork) {
+                Intent objIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(objIntent);
+            }
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setupAll();
+
+    }
+
+    private void setupAll() {
+
+        objLocationManager.removeUpdates(objLocationListener);
+        String strLat = "Unknow";
+        String strLng = "Unknow";
+
+        Location objNetworkLocation = requesUpdateFromProvider(LocationManager.NETWORK_PROVIDER, "Network not Connected");
+        if (objNetworkLocation != null) {
+            strLat = String.format("%.7f", objNetworkLocation.getLatitude());
+            strLng = String.format("%.7f", objNetworkLocation.getLongitude());
+        }
+
+        Location objGPSLocation = requesUpdateFromProvider(LocationManager.GPS_PROVIDER, "GPS not Work");
+        if (objGPSLocation != null) {
+            strLat = String.format("%.7f", objGPSLocation.getLatitude());
+            strLng = String.format("%.7f", objGPSLocation.getLongitude());
+        }
+
+        txtLat.setText(strLat);
+        txtLng.setText(strLng);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        objLocationManager.removeUpdates(objLocationListener);
+
+    }
+
+    public Location requesUpdateFromProvider(final String strProvider, String strError) {
+
+        Location objLocation = null;
+        if (objLocationManager.isProviderEnabled(strProvider)) {
+
+            objLocationManager.requestLocationUpdates(strProvider, 1000, 10, objLocationListener);
+            objLocation = objLocationManager.getLastKnownLocation(strProvider);
+
+        } else {
+            Log.d("bsru", "Error = " + strError);
+        }
+
+        return objLocation;
+    }
+
+    public final LocationListener objLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            txtLat.setText(String.format("%.7f", location.getLatitude()));
+            txtLng.setText(String.format("%.7f", location.getLongitude()));
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    private void openService() {
+        objLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        objCriteria = new Criteria();
+        objCriteria.setAltitudeRequired(false);
+        objCriteria.setBearingRequired(false);
+    }
+
+    private void bindWidget() {
+        txtLat = (TextView) findViewById(R.id.txtLat);
+        txtLng = (TextView) findViewById(R.id.txtLong);
+    }
+
+
 }//main
